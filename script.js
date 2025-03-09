699b1d68e10d127d57b76980f8f417533cac0ce2
Lester <58442807+lesterluoma@users.noreply.github.com>
2025-03-09 15:33:09 -0400

    Added game logic in script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let computerScore = 0;
-let humanScore = 0;
+function getHumanChoice() {
+  let choice = prompt("Ready? Rock, Paper, Scissors, Shoot!", "rock");
 
-function computerChoice() {
-  let choice = Math.floor(Math.random() * 2); // 0 - 2
+  return choice.toLowerCase();
+}
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3); // 0 - 2
 
   if (choice === 2) {
     return "rock";
@@ -13,28 +16,65 @@ function computerChoice() {
   }
 }
 
-function humanChoice() {
-  let choice = prompt("Ready? Rock, Paper, Scissors, Shoot!", "rock");
-
-  return choice.toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice) {
-  const winningChoice = {
-    rock: "paper",
-    paper: "scissors",
-    scissors: "rock",
-  };
-
   if (humanChoice === computerChoice) {
     return {
-      winner: "tie",
-      score: 0,
+      message: confirm(`Tie. You both chose ${humanChoice}.`),
     };
+  }
+
+  const winningChoice = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
+
+  if (winningChoice[humanChoice] === computerChoice) {
+    return {
+      message: confirm(
+        `You win! ${capitalizeFirstLetter(
+          humanChoice
+        )} beats ${computerChoice}.`
+      ),
+      winner: "human",
+    };
+  }
+
+  return {
+    message: confirm(
+      `You lose! ${capitalizeFirstLetter(computerChoice)} beats ${humanChoice}.`
+    ),
+    winner: "computer",
+  };
+}
+
+function playGame() {
+  let computerScore = 0;
+  let humanScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    let result = playRound(humanSelection, computerSelection);
+
+    if (result.winner === "human") {
+      humanScore++;
+    }
+    if (result.winner === "computer") {
+      computerScore++;
+    }
+    console.log(`humanScore: ${humanScore} computerScore: ${computerScore}`);
+  }
+
+  if (humanScore > computerScore) {
+    return confirm("Congratulations! You win!");
+  } else {
+    return confirm("Game over.");
   }
 }
 
-const humanSelection = humanChoice();
-const computerSelection = computerChoice();
+function capitalizeFirstLetter(val) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
 
-playRound(humanSelection, computerSelection);
+playGame();
